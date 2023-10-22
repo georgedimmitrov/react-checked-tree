5cfd60cd1faf7a0165ad8700a1efc60fb40a6d1c
dgeorgi <dgeorgi@vmware.com>
2023-10-22 14:05:27 +0300

    Setup for start of challenge

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+// import CheckedTree from './CheckedTree'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [formData, setFormData] = useState({ checkedTree: [] });
+
+  const handleSubmit = () => {
+    console.log(formData);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className="container">
+        <h1>Mock Form</h1>
+        <input placeholder="Mock Input" className="common" />
+        <input placeholder="Mock Input" className="common" />
+        {/* <CheckedTree /> */}
+        <button onClick={handleSubmit} className="common">
+          Submit
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/data.tsx
@@ -0,0 +1,18 @@
+export type Item = {
+  id: number;
+  parentId: number | null;
+  name: string;
+};
+
+const response: Item[] = [
+  { id: 4, parentId: 3, name: "Days" },
+  { id: 1, parentId: null, name: "Years" },
+  { id: 2, parentId: 1, name: "Months" },
+  { id: 5, parentId: null, name: "Stars" },
+  { id: 3, parentId: 2, name: "Weeks" },
+  { id: 6, parentId: 5, name: "Sun" },
+  { id: 7, parentId: 5, name: "Proxima Centauri" },
+  { id: 8, parentId: null, name: "Dogs" },
+];
+
+export default response;
